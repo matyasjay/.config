8c687eb3a03c011dc5c19f505675284cb85bd72f
Matyas Angyal <matyas.angyal@jaystack.com>
2024-03-19 10:07:39 +0100

    chore: change tmux colors to tokyonight

--- nvim/lua/amatyas/init.lua
+++ nvim/lua/amatyas/init.lua
@@ -4,13 +4,10 @@ require("amatyas.fugitive")
 require("amatyas.harpoon")
 require("amatyas.set")
 require("amatyas.prettier")
+require("amatyas.lualine")
 
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.g.netrw_browse_split = 0
 
-require('lualine').setup({
-  options = {
-    theme = 'tokyonight'
-  }
-})
+
