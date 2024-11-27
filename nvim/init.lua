4d476f155445be1b8d26142bc44cc03f82ee79e0
Matyas Angyal <matyas.angyal@jaystack.com>
2024-11-27 21:53:09 +0100

    feat: add command to Q variants

--- nvim/init.lua
+++ nvim/init.lua
@@ -63,6 +63,12 @@ end, {
 	complete = "file",
 })
 
+vim.api.nvim_create_user_command("WQ", "wq", {})
+vim.api.nvim_create_user_command("Wq", "wq", {})
+vim.api.nvim_create_user_command("W", "w", {})
+vim.api.nvim_create_user_command("Qa", "qa", {})
+vim.api.nvim_create_user_command("Q", "q", {})
+
 vim.keymap.set("n", "<C-N>", function()
 	local filename = vim.fn.input("New file: ", "", "file")
 	if filename ~= "" then
